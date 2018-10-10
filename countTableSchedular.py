729df02a3700ff9deb847c80a2701fecc6951360
TKVAIOZBLK\miyuk_000 <scrambleegg7@gmail.com>
2018-10-10 17:28:05 +0900

    add logic to check dynamodb process. if process is not found, shell scripts ends.

--- countTableSchedular.py
+++ countTableSchedular.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import os
+import shutil
 from datetime import date as dt
 
 import seaborn as sns
@@ -21,14 +22,35 @@ from ReceiptyClass import ReceiptyClass
 
 from boto3.dynamodb.conditions import Key, Attr
 
+from botocore.exceptions import ClientError
+
+def touch(path):
+    with open(path, 'a'):
+        os.utime(path, None)
+
 def connectDynamoDBTable():
 
-    dynamodb = boto3.resource('dynamodb',endpoint_url='http://localhost:8000')
-    table = dynamodb.Table('schedular')
-    print("TABLE creation date:",table.creation_date_time) 
+    filename = "./dynamo_connectionfail"
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
 
-    return table
+
+    try:
+        dynamodb = boto3.resource('dynamodb',endpoint_url='http://localhost:8000')
+        table = dynamodb.Table('schedular')
+        print("TABLE creation date:",table.creation_date_time) 
+        return table
+    except:
+        print("Connection Error")
+        touch(filename)
+        return None
+        
+        #prin(ce.response)
+
 
 table = connectDynamoDBTable()
 
-print("total table count for schedular", table.item_count)
+if table is not None:
+    print("total table count for schedular", table.item_count)
--- makeSchedule.sh
+++ makeSchedule.sh
@@ -11,6 +11,24 @@ export SCHEDULE=$BASEDIR/Schedule
 export GOOGLE=$BASEDIR/Google/py
 export LOGS=$SCHEDULE/daily.log
 
+
+echo "------------------------------------" > $LOGS
+echo "check dynamodb up and runnuing" >> $LOGS
+
+cd $SCHEDULE
+$PYTHONP $SCHEDULE/countTableSchedular.py >> $LOGS
+export FAILED=$SCHEDULE/dynamo_connectionfail
+
+if [ -f $FAILED ]; then
+    echo "Sorry, dynamodb is NOT running.." >> $LOGS
+    echo "Program ends." >> $LOGS
+    exit 0
+else
+    echo "dynamodb is up and running.." >> $LOGS
+    echo "..." >> $LOGS
+fi
+
+
 echo "------------------------------------" > $LOGS
 echo "START to create CSV from GoogleSheet" >> $LOGS
 cd $GOOGLE
