cef71613647bc021d6898a9edeca4e1b392e31ff
TKVAIOZBLK\miyuk_000 <scrambleegg7@gmail.com>
2021-05-28 12:40:06 +0900

    update for YOGA laptop

--- makeAllSchedule.sh
+++ makeAllSchedule.sh
@@ -9,10 +9,10 @@ export PYTHONP=/c/ProgramData/Anaconda3/python
 export BASEDIR=/l/ipython
 export SCHEDULE=$BASEDIR/Schedule
 export GOOGLE=$BASEDIR/Google/py
-export LOGS=$SCHEDULE/daily.log
+export LOGS=$SCHEDULE/dailyAllScheduleYOGA.log
 
 
-#echo "------------------------------------" > $LOGS
+echo "------------------------------------" > $LOGS
 #echo "check dynamodb up and runnuing" >> $LOGS
 
 #cd $SCHEDULE
@@ -29,25 +29,25 @@ export LOGS=$SCHEDULE/daily.log
 #fi
 
 
-echo "------------------------------------" > $LOGS
-echo "[makeAllSchedule.sh] START to create CSV from GoogleSheet" >> $LOGS
-cd $GOOGLE
-$PYTHONP $GOOGLE/createLGCSVFromSheet.py >> $LOGS
+#echo "------------------------------------" > $LOGS
+#echo "[makeAllSchedule.sh] START to create CSV from GoogleSheet" >> $LOGS
+#cd $GOOGLE
+#$PYTHONP $GOOGLE/createLGCSVFromSheet.py >> $LOGS
 
-echo "[makeAllSchedule.sh] insert GoogleSheet into MongoDB(cloud) " >> $LOGS
-cd $SCHEDULE
-echo "[makeAllSchedule.sh]  batch process to insert data  " >> $LOGS
+#echo "[makeAllSchedule.sh] insert GoogleSheet into MongoDB(cloud) " >> $LOGS
+#cd $SCHEDULE
+#echo "[makeAllSchedule.sh]  batch process to insert data  " >> $LOGS
 #$PYTHONP $SCHEDULE/insertLGSchedularBatch.py >> $LOGS
-$PYTHONP $SCHEDULE/insertMongoLGSchedularBatch.py >> $LOGS
+#$PYTHONP $SCHEDULE/insertMongoLGSchedularBatch.py >> $LOGS
 
-echo "[makeAllSchedule.sh] build integrate2.csv (newly added data from RECEPTY) for GoogleSheet " >> $LOGS
+echo "[makeAllSchedule.sh] build integrate2.csv " >> $LOGS
 #$PYTHONP $SCHEDULE/buildIntegrate2CSVForScheduleSheet.py >> $LOGS
 #$PYTHONP $SCHEDULE/buildMongoIntegrate2CSVForScheduleSheet.py >> $LOGS
 
 $PYTHONP $SCHEDULE/buildMongoIntegrate2CSVForScheduleSheetTobuNERIMA.py >> $LOGS
 
-echo "------------------------------------" >> $LOGS
-echo "[makeAllSchedule.sh] Append RECEPTY new data on Google Sheet." >> $LOGS
-cd $GOOGLE
+#echo "------------------------------------" >> $LOGS
+#echo "[makeAllSchedule.sh] Append RECEPTY new data on Google Sheet." >> $LOGS
+#cd $GOOGLE
 #$PYTHONP $GOOGLE/appendScheduleSheet.py >> $LOGS
 #$PYTHONP $GOOGLE/clearAndSetFilterSortScheduleSheet.py >> $LOGS
--- runBashAllSchedule.bat
+++
@@ -1,5 +0,0 @@
-cmd /c ""C:\myProgram\Git\bin\bash.exe" --login -i -- L:\ipython\Schedule\makeAllSchedule.sh"
-
-pause
-
-
